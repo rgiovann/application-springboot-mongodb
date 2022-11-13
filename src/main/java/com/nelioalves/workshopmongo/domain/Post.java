0781bc39ef6b386cb0209ced0f11b37be7cab4c3
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-11-13 12:22:35 -0300

    Adding comments to post using DTO.

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable {
@@ -17,8 +20,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
-	
+	private List<CommentDTO> comments = new ArrayList<CommentDTO>();
 	
 	public Post() {
 		super();
@@ -66,13 +68,21 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 	
-	public AuthorDTO getUser() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setUser(AuthorDTO user) {
-		this.author = user;
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
 	}	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
